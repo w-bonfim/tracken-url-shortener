73c1af768e1a3e02b4295e935187eb2a7f58fa80
Wellington Bonfim <wellington.bonfim@hotmail.com>
2026-08-19 08:51:00 -0300

    feat: adiciona CRUD das url's encurtadas

---
+++ app/Http/Controllers/Api/ShortUrlController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreShortUrlRequest;
+use App\Http\Requests\UpdateShortUrlRequest;
+use App\Http\Resources\ShortUrlResource;
+use App\Models\ShortUrl;
+use App\Services\ShortUrlService;
+use Illuminate\Http\Response;
+
+class ShortUrlController extends Controller
+{
+    public function __construct(private readonly ShortUrlService $shortUrlService)
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $shortUrls = ShortUrl::latest()->get();
+
+        return ShortUrlResource::collection($shortUrls);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShortUrlRequest $request)
+    {
+        $shortUrl = $this->shortUrlService->create($request->validated('original_url'));
+
+        return (new ShortUrlResource($shortUrl))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShortUrl $shortUrl)
+    {
+        return new ShortUrlResource($shortUrl);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShortUrlRequest $request, ShortUrl $shortUrl)
+    {
+        $shortUrl->update($request->validated());
+
+        return new ShortUrlResource($shortUrl);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShortUrl $shortUrl)
+    {
+        $shortUrl->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ShortUrlController;
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource('short-urls', ShortUrlController::class);
